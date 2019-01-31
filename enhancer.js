fc49ffbc99c8d5afac7a8c0e38696e5720584315
BryceEvans <evans.bryce@gmail.com>
2019-01-30 22:46:44 -0700

    Going slowly but got one test to pass.

--- enhancer.js
+++ enhancer.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   success: (item) => {
-
+    return item;
   },
 
   fail: (item) => {
--- enhancer.test.js
+++ enhancer.test.js
@@ -1,7 +1,15 @@
 const enhancer = require('./enhancer');
+const items = require('./items');
+
+const enhancedItems = items.map(enhancer.success);
 
 test('', () => {
-
+  expect(enhancer.success(enhancedItems[0])).toEqual({
+    name: 'shortsword',
+    type: 'weapon',
+    durability: 100,
+    enhancement: ''
+  });
 })
 
 // test('', () => {
